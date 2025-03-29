d835229560b44b22adac4dd496f6483e82fad00f
dinbecarevic <din.velenje@gmail.com>
2025-03-29 18:01:18 +0100

    implemented client

--- client/client.py
+++ client/client.py
@@ -5,14 +5,24 @@ import os
 
 app = Flask(__name__)
 
+SERVER_URL = 'http://server:5000/get_image' # server je ime Docker vsebnika, ki se razreši v pravilen IP naslov v Docker omrežju
 
 @app.route('/')
 def index():
-    # show html
+    return render_template('index.html')
 
 @app.route('/get_current_image')
 def get_current_image():
-    #return current image
+    try:
+        response = requests.get(SERVER_URL)
+        data = response.json()
+        return jsonify(data)
+    except Exception as e:
+        print(f"Error getting image: {str(e)}")
+        return jsonify({'error': str(e), 'image': None})
 
 if __name__ == '__main__':
+    print("Počakam, da bo strežnik na voljo...")
+    time.sleep(5)
+    
     app.run(host='0.0.0.0', port=5000)
